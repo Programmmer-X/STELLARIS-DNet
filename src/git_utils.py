ce282ed3a2bb1191f9cf9cbeab9ba2cb0849725d
Programmmer-X <prajwal.sutar.0999@gmail.com>
2026-03-30 22:59:36 +0000

    Added git utility module

---
+++ src/git_utils.py
@@ -0,0 +1,31 @@
+
+# my function
+
+import os
+from getpass import getpass
+
+def auto_push_notebook(repo_path="/content/STELLARIS-DNet",
+                       notebook_name=None,
+                       target_folder="notebooks/experiments",
+                       commit_msg="Auto notebook update",
+                       repo_url="https://github.com/Programmmer-X/STELLARIS-DNet.git"):
+
+    TOKEN = getpass("Enter GitHub PAT: ")
+
+    if notebook_name is None:
+        print("⚠️ Please provide notebook_name manually")
+        return
+
+    nb_path = f"/content/{notebook_name}"
+    target_path = f"{repo_path}/{target_folder}"
+
+    os.makedirs(target_path, exist_ok=True)
+
+    os.system(f'cp "{nb_path}" "{target_path}/"')
+
+    os.chdir(repo_path)
+    os.system("git add .")
+    os.system(f'git commit -m "{commit_msg}"')
+    os.system(f'git push https://{TOKEN}@github.com/Programmmer-X/STELLARIS-DNet.git main')
+
+    print("✅ Notebook pushed successfully!")
